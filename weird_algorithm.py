9e0c2af7a670a7a2564751ec04a5de0df790c2aa
obusama <eswaruce@gmail.com>
2023-05-10 18:05:27 +0530

    solution

--- weird_algorithm.py
+++ weird_algorithm.py
@@ -1,1 +1,9 @@
-print("hello wordld!")
+n = int(input("Enter an integer :"))
+
+print(n, end=" ")
+while n!=1:
+    if n%2 ==0:
+        n = int(n/2)
+    else:
+        n = int(3*n+1)
+    print(n, end=" ")
